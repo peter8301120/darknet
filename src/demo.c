7507379a95db9954be55fc4c36ccd64689fbd5a5
Tinway <B10230408@mail.ntust.edu.tw>
2020-01-07 09:11:26 +0800

    Update demo.c

--- src/demo.c
+++ src/demo.c
@@ -226,7 +226,7 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
             //printf("\033[2J");
             //printf("\033[1;1H");
             //printf("\nFPS:%.1f\n", fps);
-            printf("Objects:\n\n");
+            //printf("Objects:\n\n");
 
             ++frame_id;
             if (demo_json_port > 0) {
